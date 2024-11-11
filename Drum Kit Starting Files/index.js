00f4e577a554a0218bbd959dadf55d3da2dd3656
charancholaveti <cholaveticharan47@gmail.com>
2024-11-11 15:43:37 +0530

    drum kit project added

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,55 @@
+var numberOfButtons = document.querySelectorAll(".drum").length;
+
+for(var i=0;i<numberOfButtons;i++){
+    document.querySelectorAll('.drum')[i].addEventListener("click",function() {
+        var buttonHtml=this.innerHTML;
+        makeSound(buttonHtml);
+        buttonAnimation(buttonHtml);
+    });
+}
+
+document.addEventListener("keypress",function(event){
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+
+function makeSound(key){
+
+    switch(key){
+        case "w": var audio=new Audio('sounds/tom-1.mp3');
+                  audio.play();
+                  break;
+        case "a": var audio=new Audio('sounds/tom-2.mp3');
+                  audio.play();
+                  break;    
+        case "s": var audio=new Audio('sounds/tom-3.mp3');
+                  audio.play();
+                  break;
+        case "d": var audio=new Audio('sounds/tom-4.mp3');
+                  audio.play();
+                  break;  
+        case "j": var audio=new Audio('sounds/snare.mp3');
+                  audio.play();
+                  break; 
+        case "k": var audio=new Audio('sounds/crash.mp3');
+                  audio.play();
+                  break; 
+        case "l": var audio=new Audio('sounds/kick-bass.mp3');
+                  audio.play();
+                  break; 
+        default :console.log('buttonHtml');
+    }
+
+}
+
+function buttonAnimation(pressedKey){
+
+    var activeButton=document.querySelector("."+pressedKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+    },100);
+}
