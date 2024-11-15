b1e6ca0bdacbfd1d0eb8d6d08f3c2dee3f0623aa
charancholaveti <cholaveticharan47@gmail.com>
2024-11-15 22:28:47 +0530

    qr_code_generator

---
+++ QR-CODE-GENERATOR/index.js
@@ -0,0 +1,30 @@
+import inquirer from 'inquirer';
+// var qr = require('qr-image');
+import qr from 'qr-image';
+import fs from 'fs';
+inquirer
+  .prompt([
+    /* Pass your questions in here */
+    {
+        message:"Enter your URL to generate QR-Code:",
+        name:"URL",
+    },
+  ])
+  .then((answers) => {
+    // Use user feedback for... whatever!!
+    const url=answers.URL;
+    // console.log(url);
+    var qr_svg = qr.image(url);
+   qr_svg.pipe(fs.createWriteStream("qr.png"));
+   fs.writeFile('message.txt', url, (err) => {
+    if (err) throw err;
+    console.log('The file has been saved!');
+  }); 
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
