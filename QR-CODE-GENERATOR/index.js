6d541121946cc704bf04ca5cc52a1ef1311b6079
charancholaveti <cholaveticharan47@gmail.com>
2024-11-18 19:05:57 +0530

    newaddedfiles

--- QR-CODE-GENERATOR/index.js
+++ QR-CODE-GENERATOR/index.js
@@ -15,7 +15,7 @@ inquirer
     const url=answers.URL;
     // console.log(url);
     var qr_svg = qr.image(url);
-   qr_svg.pipe(fs.createWriteStream("qr1.png"));
+   qr_svg.pipe(fs.createWriteStream("qr4.png"));
    fs.writeFile('message.txt', url, (err) => {
     if (err) throw err;
     console.log('The file has been saved!');
